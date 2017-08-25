9ce98e2fef756efcd41c05db7bbfb6a4483be2ac
karobolas <31293743+karobolas@users.noreply.github.com>
2017-08-25 03:37:18 +0200

    Thread support!

--- main.js
+++ main.js
@@ -27,15 +27,15 @@ const token = config.token;
 let slack = new RtmClient(token, {
     // Sets the level of logging we require
     logLevel: 'info',
-    // Initialize a data store for our client, this will 
-    // load additional helper functions for the storing 
+    // Initialize a data store for our client, this will
+    // load additional helper functions for the storing
     // and retrieval of data
     dataStore: new MemoryDataStore(),
-    // Boolean indicating whether Slack should automatically 
+    // Boolean indicating whether Slack should automatically
     // reconnect after an error response
     autoReconnect: true,
-    // Boolean indicating whether each message should be marked as // read 
-    // or not after it is processed 
+    // Boolean indicating whether each message should be marked as // read
+    // or not after it is processed
     autoMark: true
 });
 
@@ -60,7 +60,7 @@ function getChannels(allChannels) {
 
 // Add an event listener for the RTM_CONNECTION_OPENED event,
 //  which is called when the bot
-// connects to a channel. The Slack API can subscribe to 
+// connects to a channel. The Slack API can subscribe to
 // events by using the 'on' method
 slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
     // Get the user's name
@@ -69,16 +69,16 @@ slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
     // Get the team's name
     let team = slack.dataStore.getTeamById(slack.activeTeamId);
 
-    // Log the slack team name and the bot's name, using ES6's 
+    // Log the slack team name and the bot's name, using ES6's
     // template string syntax
     console.log(`Connected to ${team.name} as ${user.name}`);
 
-    // Note how the dataStore object contains a list of all 
+    // Note how the dataStore object contains a list of all
     // channels available
     let channels = getChannels(slack.dataStore.channels);
 
-    // Use Array.map to loop over every instance and return an 
-    // array of the names of each channel. Then chain Array.join 
+    // Use Array.map to loop over every instance and return an
+    // array of the names of each channel. Then chain Array.join
     // to convert the names array to a string
     let channelNames = channels.map((channel) => {
         return channel.name;
@@ -99,17 +99,37 @@ slack.on(RTM_EVENTS.MESSAGE, (message) => {
 
     if (message.text) {
         let msg = message.text.toLowerCase();
-        
+
         //Return false if message startswith "<" (This is probably a automated message)
         if ((msg.startsWith("&lt;")) || (msg.startsWith("<"))){
           return;
         }
-        
+
         translate.autoToEnglish(msg)
             .then((result) => {
-                if (result.lang !== "en") {
-                    slack.sendMessage(`${user.name} said: ${result.text} :star: _TranslationBuddy, a truly multilingual partner!_`, channel.id);
+              if (result.lang !== "en") {
+                //Create payload
+                var payload = {
+                  "id": 1,
+                  "type": "message",
+                  "channel": channel.id,
+                  "text": user.real_name + ': ' + result.text + config.messagesuffix
                 }
+
+                //Check if it's a thread
+                if (message.thread_ts != undefined){
+                  //It's a thread. Include thread ts
+                  payload['thread_ts'] = message.thread_ts;
+                } else {
+                  //create new thread if true.. Else a normal message will be sent
+                  if (config.thread){
+                    payload['thread_ts'] = message.ts;
+                  }
+                }
+
+                //Send message
+                slack.send(payload);
+              }
             })
             .catch((err) => {
                 console.log(err);
